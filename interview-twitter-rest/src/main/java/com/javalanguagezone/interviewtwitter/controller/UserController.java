a263972a63f7ad47f516cc9f2861c7e6ecd43446
Jasmin <jasmin.merusic@gmail.com>
2018-11-12 15:14:07 +0100

    The setup (added missing dependencies)

--- interview-twitter-rest/src/main/java/com/javalanguagezone/interviewtwitter/controller/UserController.java
+++ interview-twitter-rest/src/main/java/com/javalanguagezone/interviewtwitter/controller/UserController.java
@@ -2,11 +2,13 @@ package com.javalanguagezone.interviewtwitter.controller;
 
 import com.javalanguagezone.interviewtwitter.service.UserService;
 import com.javalanguagezone.interviewtwitter.service.dto.UserDTO;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 import java.util.Collection;
+import java.util.HashSet;
 
 @RestController
 public class UserController {
@@ -18,12 +20,25 @@ public class UserController {
   }
 
   @GetMapping("/followers")
-  public Collection<UserDTO> followers(Principal principal) {
-    return userService.getUsersFollowers(principal);
+  public String followers(Principal principal) {
+     userService.getUsersFollowers(principal);
+    return "overview";
   }
 
   @GetMapping("/following")
   public Collection<UserDTO> following(Principal principal) {
     return userService.getUsersFollowing(principal);
   }
+
+  @GetMapping("/overview")
+  public String overview(Principal principal, Model model){
+
+    Collection<UserDTO> userDTOS=new HashSet<>();
+
+    model.addAttribute("followers",userService.getUsersFollowers(principal));
+    model.addAttribute("following",userService.loadUserByUsername(principal.toString()));
+    return "overview";
+  }
+
+
 }
